6395db572c302441104c82c3aa7fb208350a0a11
Svitlana Niesmielova <amelia-com@hotmail.com>
2021-04-14 10:59:35 +0300

    delate catch

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,7 +3,7 @@ const URL = 'https://restcountries.eu/rest/v2/name/';
 export default function fetchCountries(countryName) {
   return fetch(`${URL}${countryName}`)
     .then(r => r.json())
-    .catch(error => console.log(error));
+    // .catch(error => console.log(error));
 }
 
 // data.filter(country => country.name.includes('uk'));
